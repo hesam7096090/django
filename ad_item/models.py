b3ac189079f32d05b54da94b037b863c8815cce2
hesam7096090 <rahimizadehhesam@gmail.com>
2025-01-14 18:51:42 +0330

    change password

--- ad_item/models.py
+++ ad_item/models.py
@@ -42,8 +42,8 @@ class Item(models.Model):
 
 
 class Collateral(models.Model):
-    name = models.CharField(max_length=100)  # نام وثیقه
-    description = models.TextField(blank=True, null=True)  # توضیحات اختیاری
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
